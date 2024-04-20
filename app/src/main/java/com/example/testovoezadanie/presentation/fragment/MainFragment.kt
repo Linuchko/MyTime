a2c8ff2afcae263563c4f28009236b8743bf3d39
Linuchko <lina3sycheva@gmail.com>
2024-04-20 21:33:30 +0300

    Initial commit

--- app/src/main/java/com/example/testovoezadanie/presentation/fragment/MainFragment.kt
+++ app/src/main/java/com/example/testovoezadanie/presentation/fragment/MainFragment.kt
@@ -1,31 +1,21 @@
 package com.example.testovoezadanie.presentation.fragment
 
-import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import com.airbnb.epoxy.EpoxyController
 import com.airbnb.mvrx.*
 import com.example.testovoezadanie.R
-import com.example.testovoezadanie.fragmentMain
-import com.example.testovoezadanie.presentation.helper.MyEpoxyController
+import com.example.testovoezadanie.presentation.helper.MyEpController
 import com.example.testovoezadanie.presentation.helper.fragmentCustomModel
 import kotlinx.android.synthetic.main.epoxy_fragment_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import java.util.*
 
 
 class MainFragment : BaseMvRxFragment(){
 
     private val viewModel: MainFragmentViewModel by fragmentViewModel()
-    private val epoxyController: MyEpoxyController by lazy{MyEpoxyController(requireContext())}
+    private val epoxyController: MyEpController by lazy{MyEpController(requireContext())}
 
     companion object{
         private const val TAG = "MainFragment"
